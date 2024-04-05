edfd87e9f6dddd534a09337783e56b4fe25f18eb
devitoangelo <angelodevito4@gmail.com>
2024-04-05 14:36:04 +0200

    create a function movie and series

--- src/state.js
+++ src/state.js
@@ -8,22 +8,44 @@ export const state = reactive({
     FilmMovie: 'search/movie',
     FilmSeries: 'search/tv',
     api_key: '2df89758a34ce45fc10402fa4da73b3a&',
-    films: [],
     search: '',
+    films: {
+        movies: {},
+        tv: {}
+    },
 
 
-    callApi(url){
-        axios.get(url)
-        .then(resp => {
-            console.log(resp);
-            this.films = resp.data.results
-        })
-        .catch(err => {
-            console.log(err);
-        })
 
 
-    }
+    Movie(){
+        const url = `${this.urlType + this.FilmMovie}?api_key=${this.api_key}&query=${this.search}`
+        return axios.get(url)
+
+    },
+
+    Series(){
+        const url = `${this.urlType + this.FilmSeries}?api_key=${this.api_key}&query=${this.search}`
+        return axios.get(url)
+
+    },
+
+
+
+
+
+
+    // callApi(url){
+    //     axios.get(url)
+    //     .then(resp => {
+    //         console.log(resp);
+    //         this.films = resp.data.results
+    //     })
+    //     .catch(err => {
+    //         console.log(err);
+    //     })
+
+
+    // }
 
 
 
